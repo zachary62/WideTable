7b5931e69060312bce9c35964f6c57c42da42fbe
Zachary Huang <zach@mltrain4.byvynejmqzpurg20o52my20h0f.bx.internal.cloudapp.net>
2022-11-12 22:34:42 +0000

    revert test

--- src/test_cjt.py
+++ src/test_cjt.py
@@ -42,98 +42,98 @@ class TestCJT(unittest.TestCase):
         cjt.add_join('S', 'T', ['F'], ['F']);
         return cjt
 
-#     """
-#     Tests if message passing works in many to many join graph.
-#     Following tables have many to many cardinality on B.
-#     Test Query:
-#         SELECT SUM(A), count(*), R.B
-#         FROM R join G on R.B = G.B join T on R.B = T.B
-#         GROUP BY R.B ORDER BY R.B
-#     """
-#     def test_many_to_many(self):
-#         cjt = self.initialize_synthetic_many_to_many(semi_ring=AvgSemiRing(relation='R', attr='A'))
-#         expected = cjt.exe.conn.execute(
-#             """
-#             SELECT SUM(A), count(*), R.B 
-#             FROM R join S on R.B = S.B join T on R.B = T.B 
-#             GROUP BY R.B ORDER BY R.B
-#             """).fetchall()
-#         cjt.lift_all()
-#         cjt.calibration()
-#         actual = cjt.absorption('T', ['B'], ['B'], mode=3)
-#         self.assertEqual(actual, expected)
+    """
+    Tests if message passing works in many to many join graph.
+    Following tables have many to many cardinality on B.
+    Test Query:
+        SELECT SUM(A), count(*), R.B
+        FROM R join G on R.B = G.B join T on R.B = T.B
+        GROUP BY R.B ORDER BY R.B
+    """
+    def test_many_to_many(self):
+        cjt = self.initialize_synthetic_many_to_many(semi_ring=AvgSemiRing(relation='R', attr='A'))
+        expected = cjt.exe.conn.execute(
+            """
+            SELECT SUM(A), count(*), R.B 
+            FROM R join S on R.B = S.B join T on R.B = T.B 
+            GROUP BY R.B ORDER BY R.B
+            """).fetchall()
+        cjt.lift_all()
+        cjt.calibration()
+        actual = cjt.absorption('T', ['B'], ['B'], mode=3)
+        self.assertEqual(actual, expected)
 
-#     """
-#     Tests if message passing works in one to many join graph. 
-#     Similar to star schema with Fact Table. Here, T is the fact table
-#     Test Query:
-#         SELECT SUM(K), count(*), R.B 
-#         FROM R join T on R.B = T.B join S on S.F = T.F 
-#         GROUP BY T.B ORDER BY T.B
-#     """
-#     def test_one_to_many(self):
-#         cjt = self.initialize_synthetic_one_to_many(semi_ring=AvgSemiRing(relation='T', attr='K'))
-#         expected = cjt.exe.conn.execute(
-#             """
-#             SELECT SUM(T.K), count(*), T.B 
-#             FROM R join T on R.B = T.B join S on S.F = T.F 
-#             GROUP BY T.B ORDER BY T.B
-#             """
-#         ).fetchall()
-#         cjt.lift_all()
-#         cjt.calibration()
-#         actual = cjt.absorption('T', ['B'], ['B'], mode=3)
-#         self.assertEqual(actual, expected)
+    """
+    Tests if message passing works in one to many join graph. 
+    Similar to star schema with Fact Table. Here, T is the fact table
+    Test Query:
+        SELECT SUM(K), count(*), R.B 
+        FROM R join T on R.B = T.B join S on S.F = T.F 
+        GROUP BY T.B ORDER BY T.B
+    """
+    def test_one_to_many(self):
+        cjt = self.initialize_synthetic_one_to_many(semi_ring=AvgSemiRing(relation='T', attr='K'))
+        expected = cjt.exe.conn.execute(
+            """
+            SELECT SUM(T.K), count(*), T.B 
+            FROM R join T on R.B = T.B join S on S.F = T.F 
+            GROUP BY T.B ORDER BY T.B
+            """
+        ).fetchall()
+        cjt.lift_all()
+        cjt.calibration()
+        actual = cjt.absorption('T', ['B'], ['B'], mode=3)
+        self.assertEqual(actual, expected)
 
-#     """
-#     Tests if message passing works in one to many join graph with selection.
-#     Similar to star schema with Fact Table. Here, T is the fact table
-#     Test Query:
-#         SELECT SUM(K), count(*), R.B 
-#         FROM R join T on R.B = T.B join S on S.F = T.F 
-#         WHERE S.E = 2
-#         GROUP BY T.B ORDER BY T.B
-#     """
-#     def test_one_to_many_with_selection(self):
-#         cjt = self.initialize_synthetic_one_to_many(semi_ring=AvgSemiRing(relation='T', attr='K'))
-#         expected = cjt.exe.conn.execute(
-#             """
-#             SELECT SUM(T.K), count(*), T.B 
-#             FROM R join T on R.B = T.B join S on S.F = T.F 
-#             WHERE S.E = 2
-#             GROUP BY T.B ORDER BY T.B
-#             """
-#         ).fetchall()
-#         cjt.add_annotations('S', ['E', Annotation.NOT_DISTINCT, '2'])
-#         cjt.lift_all()
-#         cjt.calibration()
-#         actual = cjt.absorption('T', ['B'], ['B'], mode=3)
-#         self.assertEqual(actual, expected)
+    """
+    Tests if message passing works in one to many join graph with selection.
+    Similar to star schema with Fact Table. Here, T is the fact table
+    Test Query:
+        SELECT SUM(K), count(*), R.B 
+        FROM R join T on R.B = T.B join S on S.F = T.F 
+        WHERE S.E = 2
+        GROUP BY T.B ORDER BY T.B
+    """
+    def test_one_to_many_with_selection(self):
+        cjt = self.initialize_synthetic_one_to_many(semi_ring=AvgSemiRing(relation='T', attr='K'))
+        expected = cjt.exe.conn.execute(
+            """
+            SELECT SUM(T.K), count(*), T.B 
+            FROM R join T on R.B = T.B join S on S.F = T.F 
+            WHERE S.E = 2
+            GROUP BY T.B ORDER BY T.B
+            """
+        ).fetchall()
+        cjt.add_annotations('S', ['E', Annotation.NOT_DISTINCT, '2'])
+        cjt.lift_all()
+        cjt.calibration()
+        actual = cjt.absorption('T', ['B'], ['B'], mode=3)
+        self.assertEqual(actual, expected)
 
-#     """
-#     Tests if message passing works in many to many join graph.
-#     Following tables have many to many cardinality on B.
-#     Test Query:
-#         SELECT SUM(A), count(*), R.B
-#         FROM R join G on R.B = G.B join T on R.B = T.B
-#         WHERE G.B = 2
-#         GROUP BY R.B ORDER BY R.B
-#     """
-#     def test_many_to_many_with_selection(self):
-#         cjt = self.initialize_synthetic_many_to_many(semi_ring=AvgSemiRing(relation='R', attr='A'))
-#         expected = cjt.exe.conn.execute(
-#             """
-#             SELECT SUM(A), count(*), R.B 
-#             FROM R join S on R.B = S.B join T on R.B = T.B 
-#             WHERE S.B = 2
-#             GROUP BY R.B ORDER BY R.B
-#             """).fetchall()
+    """
+    Tests if message passing works in many to many join graph.
+    Following tables have many to many cardinality on B.
+    Test Query:
+        SELECT SUM(A), count(*), R.B
+        FROM R join G on R.B = G.B join T on R.B = T.B
+        WHERE G.B = 2
+        GROUP BY R.B ORDER BY R.B
+    """
+    def test_many_to_many_with_selection(self):
+        cjt = self.initialize_synthetic_many_to_many(semi_ring=AvgSemiRing(relation='R', attr='A'))
+        expected = cjt.exe.conn.execute(
+            """
+            SELECT SUM(A), count(*), R.B 
+            FROM R join S on R.B = S.B join T on R.B = T.B 
+            WHERE S.B = 2
+            GROUP BY R.B ORDER BY R.B
+            """).fetchall()
         
-#         cjt.add_annotations('S', ['B', Annotation.NOT_DISTINCT, '2'])
-#         cjt.lift_all()
-#         cjt.calibration()
-#         actual = cjt.absorption('T', ['B'], ['B'], mode=3)
-#         self.assertEqual(actual, expected)
+        cjt.add_annotations('S', ['B', Annotation.NOT_DISTINCT, '2'])
+        cjt.lift_all()
+        cjt.calibration()
+        actual = cjt.absorption('T', ['B'], ['B'], mode=3)
+        self.assertEqual(actual, expected)
         
     # TODO: add more tests for group-by
     def test_one_to_many_with_groupby(self):
