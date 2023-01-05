7ea07ff4d96153624810b674992a6c4b845ed740
Pavan Kalyan <damalapati.pavan@gmail.com>
2023-01-05 15:48:16 -0500

    add measurement functionality

--- src/static/main.js
+++ src/static/main.js
@@ -4,9 +4,10 @@ import SchemaView from "./views/schemaView.js";
 import VizualizationController from "./controllers/vizualizationController.js";
 import VisView from "./views/visView.js";
 import HistView from "./views/histView.js";
+import MeasurementView from "./views/measurementView.js";
 
 
-async function get_graph() {
+export async function get_graph() {
     return fetch('/get_graph')
         .then(response => response.json())
         .then(data => {return data });
@@ -14,11 +15,12 @@ async function get_graph() {
 
 async function build_graph() {
     let graph = await get_graph();
-    let tableController = new VizualizationController(graph, 
-        new JoinGraphView(d3.select('#graphView')), 
+    let tableController = new VizualizationController(graph,
+        new JoinGraphView(d3.select('#graphView')),
         new SchemaView(d3.select('#schemaView')),
         new VisView(d3.select('#visView')),
-        new HistView(d3.select('#histView'))
+        new HistView(d3.select('#histView')),
+        new MeasurementView(d3.select('#measurementView'))
     );
 }
 
