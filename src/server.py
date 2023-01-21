05703690c4403e5c3ae71d2d9ab84ef86fbaa2b7
Zachary Huang <zhuang333@wisc.edu>
2023-01-21 20:59:56 +0000

    add comments

--- src/server.py
+++ src/server.py
@@ -14,83 +14,99 @@ from widetable.scope import *
 
 app = Flask(__name__)
 
-# duck_db_conn = duckdb.connect(database=':memory:')
-# join_graph = JoinGraph(duck_db_conn)
-# dashboard = DashBoard(join_graph)
-# dashboard.add_relation('orders', relation_address='../data/tpch_10mb/orders.parquet')
-# dashboard.add_relation('lineitem', relation_address='../data/tpch_10mb/lineitem.parquet')
-# dashboard.add_relation('partsupp', relation_address='../data/tpch_10mb/partsupp.parquet')
-# dashboard.add_relation('part', relation_address='../data/tpch_10mb/part.parquet')
-# dashboard.add_relation('nation', relation_address='../data/tpch_10mb/nation.parquet')
-# dashboard.add_relation('supplier', relation_address='../data/tpch_10mb/supplier.parquet')
-# dashboard.add_relation('region', relation_address='../data/tpch_10mb/region.parquet')
-# dashboard.add_relation('customer', relation_address='../data/tpch_10mb/customer.parquet')
-#
-# dashboard.add_join('lineitem', 'orders', ['l_orderkey'], ['o_orderkey'])
-# dashboard.add_join('orders', 'customer', ['o_custkey'], ['c_custkey'])
-# dashboard.add_join('partsupp', 'supplier', ['ps_suppkey'], ['s_suppkey'])
-# dashboard.add_join('customer', 'nation', ['c_nationkey'], ['n_nationkey'])
-# dashboard.add_join('nation', 'region', ['n_regionkey'], ['r_regionkey'])
-# dashboard.add_join('lineitem', 'partsupp', ['l_suppkey', 'l_partkey'], ['ps_suppkey', 'ps_partkey'])
-# dashboard.add_join('partsupp', 'part', ['ps_partkey'], ['p_partkey'])
-# dashboard.register_measurement("sum", 'part', 'p_retailprice', scope=ReplicateFact('part', 'part'))
-# dashboard.register_measurement("sum", 'lineitem', 'l_extendedprice * (1 - l_discount)',
-#                                scope=ReplicateFact('lineitem', 'lineitem'))
-duck_db_conn = duckdb.connect(database=':memory:')
-join_graph = JoinGraph(duck_db_conn)
-dashboard = DashBoard(join_graph)
-# dashboard.add_relation('allergies', relation_address='../data/synthea/allergies.csv')
-# dashboard.add_relation('careplans', relation_address='../data/synthea/careplans.csv')
-# dashboard.add_relation('claims_transactions', relation_address='../data/synthea/claims_transactions.csv')
-# dashboard.add_relation('claims', relation_address='../data/synthea/claims.csv')
-# dashboard.add_relation('conditions', relation_address='../data/synthea/conditions.csv')
-# dashboard.add_relation('devices', relation_address='../data/synthea/devices.csv')
-dashboard.add_relation('encounters', relation_address='../data/synthea/encounters.csv')
-dashboard.add_relation('imaging_studies', relation_address='../data/synthea/imaging_studies.csv')
-# dashboard.add_relation('immunizations', relation_address='../data/synthea/immunizations.csv')
-dashboard.add_relation('medications', relation_address='../data/synthea/medications.csv')
-# dashboard.add_relation('observations', relation_address='../data/synthea/observations.csv')
-dashboard.add_relation('organizations', relation_address='../data/synthea/organizations.csv')
-dashboard.add_relation('patients', relation_address='../data/synthea/patients.csv')
-# dashboard.add_relation('payer_transitions', relation_address='../data/synthea/payer_transitions.csv')
-dashboard.add_relation('payers', relation_address='../data/synthea/payers.csv')
-dashboard.add_relation('procedures', relation_address='../data/synthea/procedures.csv')
-dashboard.add_relation('providers', relation_address='../data/synthea/providers.csv')
-# dashboard.add_relation('supplies', relation_address='../data/synthea/supplies.csv')
+def generate_tpch_dashboard():
+    duck_db_conn = duckdb.connect(database=':memory:')
+    join_graph = JoinGraph(duck_db_conn)
+    dashboard = DashBoard(join_graph)
+    dashboard.add_relation(
+        'orders', relation_address='../data/tpch_10mb/orders.parquet')
+    dashboard.add_relation(
+        'lineitem', relation_address='../data/tpch_10mb/lineitem.parquet')
+    dashboard.add_relation(
+        'partsupp', relation_address='../data/tpch_10mb/partsupp.parquet')
+    dashboard.add_relation(
+        'part', relation_address='../data/tpch_10mb/part.parquet')
+    dashboard.add_relation(
+        'nation', relation_address='../data/tpch_10mb/nation.parquet')
+    dashboard.add_relation(
+        'supplier', relation_address='../data/tpch_10mb/supplier.parquet')
+    dashboard.add_relation(
+        'region', relation_address='../data/tpch_10mb/region.parquet')
+    dashboard.add_relation(
+        'customer', relation_address='../data/tpch_10mb/customer.parquet')
 
-# dashboard.add_join('allergies', 'patients', ['PATIENT'], ['Id']);
-# dashboard.add_join('careplans', 'patients', ['PATIENT'], ['Id']);
-dashboard.add_join('procedures', 'patients', ['PATIENT'], ['Id']);
-dashboard.add_join('providers', 'organizations', ['ORGANIZATION'], ['Id']);
-# dashboard.add_join('supplies', 'patients', ['PATIENT'], ['Id']);
-# dashboard.add_join('observations', 'patients', ['PATIENT'], ['Id']);
-dashboard.add_join('encounters', 'patients', ['PATIENT'], ['Id']);
-dashboard.add_join('encounters', 'organizations', ['ORGANIZATION'], ['Id']);
-dashboard.add_join('encounters', 'providers', ['PROVIDER'], ['Id']);
-dashboard.add_join('encounters', 'payers', ['PAYER'], ['Id']);
-dashboard.add_join('imaging_studies', 'encounters', ['ENCOUNTER'], ['Id']);
-# dashboard.add_join('allergies', 'encounters', ['ENCOUNTER'], ['Id']);
-# dashboard.add_join('observations', 'encounters', ['ENCOUNTER'], ['Id']);
-dashboard.add_join('procedures', 'encounters', ['ENCOUNTER'], ['Id']);
-# dashboard.add_join('careplans', 'encounters', ['ENCOUNTER'], ['Id']);
-dashboard.add_join('imaging_studies', 'patients', ['PATIENT'], ['Id']);
-dashboard.add_join('imaging_studies', 'procedures', ['PROCEDURE_CODE'], ['CODE']);
-# dashboard.add_join('claims_transactions', 'providers', ['PROVIDERID'], ['Id']);
-# dashboard.add_join('claims_transactions', 'patients', ['PATIENTID'], ['Id']);
-# dashboard.add_join('conditions', 'encounters', ['ENCOUNTER'], ['Id']);
-# dashboard.add_join('conditions', 'patients', ['PATIENT'], ['Id']);
-# dashboard.add_join('devices', 'encounters', ['ENCOUNTER'], ['Id']);
-# dashboard.add_join('devices', 'patients', ['PATIENT'], ['Id']);
-# dashboard.add_join('immunizations', 'encounters', ['ENCOUNTER'], ['Id']);
-# dashboard.add_join('immunizations', 'patients', ['PATIENT'], ['Id']);
-dashboard.add_join('medications', 'encounters', ['ENCOUNTER'], ['Id']);
-dashboard.add_join('medications', 'patients', ['PATIENT'], ['Id']);
-dashboard.add_join('medications', 'payers', ['PAYER'], ['Id']);
-# dashboard.add_join('claims', 'patients', ['PATIENTID'], ['Id']);
-# dashboard.add_join('claims', 'providers', ['PROVIDERID'], ['Id']);
-# dashboard.add_join('payer_transitions', 'patients', ['PATIENT'], ['Id']);
-# dashboard.add_join('payer_transitions', 'payers', ['PAYER'], ['Id']);
+    dashboard.add_join('lineitem', 'orders', ['l_orderkey'], ['o_orderkey'])
+    dashboard.add_join('orders', 'customer', ['o_custkey'], ['c_custkey'])
+    dashboard.add_join('partsupp', 'supplier', ['ps_suppkey'], ['s_suppkey'])
+    dashboard.add_join('customer', 'nation', ['c_nationkey'], ['n_nationkey'])
+    dashboard.add_join('nation', 'region', ['n_regionkey'], ['r_regionkey'])
+    dashboard.add_join('lineitem', 'partsupp', ['l_suppkey', 'l_partkey'], [
+                       'ps_suppkey', 'ps_partkey'])
+    dashboard.add_join('partsupp', 'part', ['ps_partkey'], ['p_partkey'])
+    dashboard.register_measurement(
+        "sum", 'part', 'p_retailprice', scope=ReplicateFact('part', 'part'))
+    dashboard.register_measurement("sum", 'lineitem', 'l_extendedprice * (1 - l_discount)',
+                                   scope=ReplicateFact('lineitem', 'lineitem'))
+    return dashboard
 
+def generate_synthea_dashboard():
+    duck_db_conn = duckdb.connect(database=':memory:')
+    join_graph = JoinGraph(duck_db_conn)
+    dashboard = DashBoard(join_graph)
+    # dashboard.add_relation('allergies', relation_address='../data/synthea/allergies.csv')
+    # dashboard.add_relation('careplans', relation_address='../data/synthea/careplans.csv')
+    # dashboard.add_relation('claims_transactions', relation_address='../data/synthea/claims_transactions.csv')
+    # dashboard.add_relation('claims', relation_address='../data/synthea/claims.csv')
+    # dashboard.add_relation('conditions', relation_address='../data/synthea/conditions.csv')
+    # dashboard.add_relation('devices', relation_address='../data/synthea/devices.csv')
+    dashboard.add_relation('encounters', relation_address='../data/synthea/encounters.csv')
+    dashboard.add_relation('imaging_studies', relation_address='../data/synthea/imaging_studies.csv')
+    # dashboard.add_relation('immunizations', relation_address='../data/synthea/immunizations.csv')
+    dashboard.add_relation('medications', relation_address='../data/synthea/medications.csv')
+    # dashboard.add_relation('observations', relation_address='../data/synthea/observations.csv')
+    dashboard.add_relation('organizations', relation_address='../data/synthea/organizations.csv')
+    dashboard.add_relation('patients', relation_address='../data/synthea/patients.csv')
+    # dashboard.add_relation('payer_transitions', relation_address='../data/synthea/payer_transitions.csv')
+    dashboard.add_relation('payers', relation_address='../data/synthea/payers.csv')
+    dashboard.add_relation('procedures', relation_address='../data/synthea/procedures.csv')
+    dashboard.add_relation('providers', relation_address='../data/synthea/providers.csv')
+    # dashboard.add_relation('supplies', relation_address='../data/synthea/supplies.csv')
+
+    # dashboard.add_join('allergies', 'patients', ['PATIENT'], ['Id']);
+    # dashboard.add_join('careplans', 'patients', ['PATIENT'], ['Id']);
+    dashboard.add_join('procedures', 'patients', ['PATIENT'], ['Id']);
+    dashboard.add_join('providers', 'organizations', ['ORGANIZATION'], ['Id']);
+    # dashboard.add_join('supplies', 'patients', ['PATIENT'], ['Id']);
+    # dashboard.add_join('observations', 'patients', ['PATIENT'], ['Id']);
+    dashboard.add_join('encounters', 'patients', ['PATIENT'], ['Id']);
+    dashboard.add_join('encounters', 'organizations', ['ORGANIZATION'], ['Id']);
+    dashboard.add_join('encounters', 'providers', ['PROVIDER'], ['Id']);
+    dashboard.add_join('encounters', 'payers', ['PAYER'], ['Id']);
+    dashboard.add_join('imaging_studies', 'encounters', ['ENCOUNTER'], ['Id']);
+    # dashboard.add_join('allergies', 'encounters', ['ENCOUNTER'], ['Id']);
+    # dashboard.add_join('observations', 'encounters', ['ENCOUNTER'], ['Id']);
+    dashboard.add_join('procedures', 'encounters', ['ENCOUNTER'], ['Id']);
+    # dashboard.add_join('careplans', 'encounters', ['ENCOUNTER'], ['Id']);
+    dashboard.add_join('imaging_studies', 'patients', ['PATIENT'], ['Id']);
+    dashboard.add_join('imaging_studies', 'procedures', ['PROCEDURE_CODE'], ['CODE']);
+    # dashboard.add_join('claims_transactions', 'providers', ['PROVIDERID'], ['Id']);
+    # dashboard.add_join('claims_transactions', 'patients', ['PATIENTID'], ['Id']);
+    # dashboard.add_join('conditions', 'encounters', ['ENCOUNTER'], ['Id']);
+    # dashboard.add_join('conditions', 'patients', ['PATIENT'], ['Id']);
+    # dashboard.add_join('devices', 'encounters', ['ENCOUNTER'], ['Id']);
+    # dashboard.add_join('devices', 'patients', ['PATIENT'], ['Id']);
+    # dashboard.add_join('immunizations', 'encounters', ['ENCOUNTER'], ['Id']);
+    # dashboard.add_join('immunizations', 'patients', ['PATIENT'], ['Id']);
+    dashboard.add_join('medications', 'encounters', ['ENCOUNTER'], ['Id']);
+    dashboard.add_join('medications', 'patients', ['PATIENT'], ['Id']);
+    dashboard.add_join('medications', 'payers', ['PAYER'], ['Id']);
+    # dashboard.add_join('claims', 'patients', ['PATIENTID'], ['Id']);
+    # dashboard.add_join('claims', 'providers', ['PROVIDERID'], ['Id']);
+    # dashboard.add_join('payer_transitions', 'patients', ['PATIENT'], ['Id']);
+    # dashboard.add_join('payer_transitions', 'payers', ['PAYER'], ['Id']);
+    return dashboard
+
+dashboard = generate_synthea_dashboard()
 
 @app.route('/')
 def homepage():
@@ -118,33 +134,89 @@ def add_measurement():
 
 @app.route('/get_relation_sample', methods=['POST'])
 def get_relation_sample():
-    print(request)
-    # Get the string from the request data
+    # Get the JSON data from the request
     data = request.get_json()
+    print(data)
 
+    # Get the relation from the data
     relation = data["relation"]
+
+    # Get the aggregate expressions from the data, if present
     agg_exprs = data.get("agg_exprs", None)
-    # converting between text to python enum
+
+    print(agg_exprs)
+
+    # If aggregate expressions are present, convert them from text to python enums
     if agg_exprs is not None:
         for k, v in agg_exprs.items():
             agg_exprs[k] = (v[0], Aggregator[v[1]])
-    selection_conds = data["selection_conds"]
-    groupby_conds = data.get("groupby_conds") or []
-    orderby_conds = data.get("orderby_conds") or []
-    custom_order_pref = data.get("custom_order_pref") or []
+
+    selection_conds = data.get("selection_conds")
+    groupby_conds = data.get("groupby_conds")
+    orderby_conds = data.get("orderby_conds")
+    custom_order_pref = data.get("custom_order_pref")
     limit = data.get("limit", None)
+
     # Return the sample data
-    temp = dashboard.get_relation_sample(relation, selection_conds, groupby_conds, orderby_conds, agg_exprs, limit,
-                                      custom_order_pref)
+    temp = dashboard.get_relation_sample(relation, selection_conds, groupby_conds, 
+                                        orderby_conds, agg_exprs, limit, custom_order_pref)
+
     # We need a better way to handle NaNs while converting to JSON
     d = simplejson.dumps(temp, ignore_nan=True, default=datetime.isoformat)
     response = app.response_class(
         response=d,
-        status=200,mimetype='application/json')
+        status=200, mimetype='application/json')
     return response
+
+    # return jsonify(
+    #     dashboard.get_relation_sample(relation, selection_conds, groupby_conds, orderby_conds, agg_exprs, limit))
+
+
+'''
+This function returns the graph from dashboard in the format of:
+node structure:
+nodes: [
+    { 
+        id: relation,
+        attributes: [dim1, dim2, join_key1, join_key2, measurement1, measurement2],
+        join_keys: [
+            {
+                key: col1
+                multiplicity: many/one
+            },
+        ],
+        measurements: [ 
+        {
+            name: AVG(A,..),
+            relations: [
+            {name: t1, should_highlight: True/False, color: None},
+            {name: t2, should_highlight: True/False, color: None}],
+            edges: [
+            {left_rel: t1, right_rel: t2, should_highlight: True/False, color: None},
+            ]
+        }, 
+        {..}
+        ]
+    }
+]
+
+edge structure: [
+    {
+        source: node_id,
+        left_keys: [key1, key2, ...],
+        dest: node_id,
+        right_keys: [key1, key2, ...],
+    }
+]
+
+Edge and relation also stores:
+        highlight: true/false (control the opacity)
+        color: black by default
+These two can be updated in js function through interaction
+'''
 
 
 @app.route('/get_graph')
 def get_graph():
-    nodes, links = dashboard.get_graph()
-    return jsonify({'nodes': nodes, 'links': links})
+    nodes, edges = dashboard.get_graph()
+    return jsonify({'nodes': nodes, 'links': edges})
--- src/static/controllers/vizualizationController.js
+++ src/static/controllers/vizualizationController.js
@@ -1,33 +1,53 @@
-import {get_graph} from "../main.js";
+import { get_graph } from "../main.js";
 
 export default class VizualizationController {
-
+    // Initialization function that draws the web page given the join graph data and views classes
     constructor(graph, jGView, schemaView, visView, histView, measView) {
+        // The graph is from the server and contains the schema-level information of the nodes and edges of the join graph
+        this.graph = graph;
+
+        // Create views for the join graph, schema side bar, measurement side bar, visualization, histogram, and 
         this.jGView = jGView;
         this.schemaView = schemaView;
-        this.visView = visView
-        this.jGView.addDragHandlers(this.dragstarted, this.dragged, this.nodeDragEnded, this.edgeDragEnded);
-        this.jGView.addBackgroundClickHandler(this.backgroundClickHandler);
-        this.jGView.drawGraph(graph);
-        this.schemaView.addClickHandler(this.schemaClickHandler);
-        this.schemaView.addAttributeClickHandler(this.attrClickHandler);
-        this.schemaView.drawSchema(graph);
-        this.graph = graph;
-        this.histView = histView;
         this.measView = measView;
-        this.measView.addSubmitHandler(this.measSubmitHandler);
-        this.measView.addSelectionChangeHandler(this.measSelectionChangeHandler);
-        this.measView.initializeForm(graph);
-    }
+        this.visView = visView;
+        this.histView = histView;
 
+        // These are for the drag events of join graph node and edge
+        this.jGView.addDragHandlers(this.dragstarted, 
+                                    this.dragged, 
+                                    this.nodeDragEnded, 
+                                    this.edgeDragEnded);
+        // These are for the event of clicking the background
+        this.jGView.addBackgroundClickHandler(this.backgroundClickHandler);
+        // Draw the join graph based on the graph
+        this.jGView.drawGraph(graph);
+
+        // Add handler when clicking on a table schema
+        this.schemaView.addClickHandler(this.schemaClickHandler);
+        // Add handler when clicking on a table attribute
+        this.schemaView.addAttributeClickHandler(this.attrClickHandler);
+        // Draw the schema based on the graph
+        this.schemaView.drawSchema(graph);
+
+        // Add handler when clicking on the submit measurement button
+        this.measView.addSubmitHandler(this.measSubmitHandler);
+        // Add handler when changing the selection
+        this.measView.addSelectionChangeHandler(this.measSelectionChangeHandler);
+        // Initialize the measurement form based on the graph
+        this.measView.initializeForm(graph);
+}
+
+
+    // update the views when the join graph has been updated
     refreshAllViewsWithLatestGraph(graph) {
         this.graph = graph;
         this.jGView.drawGraph(graph);
         this.schemaView.drawSchema(graph);
         this.measView.initializeForm(graph);
-
     }
 
+    // given the relation Id, return the edge id in the join graph 
     getEdges(relationId) {
         let links = this.graph["links"]
         return links.filter(link => link.source.id === relationId || link.target.id === relationId)
@@ -41,8 +61,8 @@ export default class VizualizationController {
         // in case we need to handle this in a common way
     }
 
-    getData = async (relation, selection_conds=[], aggregate_exprs=null, groupby_conds=null,
-                     orderby_conds=null, limit=100, custom_order_pref= null) => {
+    getData = async (relation, selection_conds = [], aggregate_exprs = null, groupby_conds = [],
+        orderby_conds = [], limit = 10000, custom_order_pref = []) => {
         let input = {
             relation: relation,
             selection_conds: selection_conds,
@@ -64,12 +84,16 @@ export default class VizualizationController {
         return data
     }
 
-    getHistData = async (relation, attr) => {
-        let aggregate_expressions = {'count':[`${attr}`, 'COUNT'], attr:[`${attr}`, 'IDENTITY']}
+    getHistData = async (relation, attr, limit = 5) => {
+        let aggregate_expressions = { 
+            'count': [`${attr}`, 'COUNT'], 
+            attr: [`${attr}`, 'IDENTITY'] 
+        }
         let groupby_conds = [`${attr}`]
         let orderby_conds = [`COUNT(${attr}) DESC`]
-        return await this.getData(relation, [], aggregate_expressions, groupby_conds, orderby_conds,5)
+        return await this.getData(relation, undefined, aggregate_expressions, groupby_conds, orderby_conds, limit)
     }
+
     addMeasurement = async (relation, attr, scope, agg) => {
         let input = {
             relation: relation,
@@ -84,23 +108,26 @@ export default class VizualizationController {
             },
             body: JSON.stringify(input)
         }).then(response => response.json())
-            .then(data => {return data; });
+            .then(data => { return data; });
         return data
     }
 
     nodeDragEnded = async (d) => {
+        // highlight the relation dragged
         this.schemaView.unHighlight()
         this.schemaView.highlightRelationSchema(d.id)
 
+        // visualize the table of the relation
         this.visView.clear()
         let tablename = d["name"]
         let orderby_conds = d["join_keys"];
-        let data = await this.getData(tablename,[], null, null, orderby_conds, null)
+        let data = await this.getData(tablename, undefined, undefined, undefined, orderby_conds)
 
         let links = this.getEdges(d["id"])
         this.visView.clear()
         this.visView.drawSingleTable(tablename, data["header"], data["data"], links, null, null, this.exploreHandler)
     }
+    
     edgeDragEnded = (d) => {
         this.schemaView.unHighlight()
         d.left_keys[0].map(att => this.schemaView.highlightRelationAttribute(d.source.id, att));
@@ -129,7 +156,7 @@ export default class VizualizationController {
 
     //     let links1 = this.getEdges(tablename)
     //     let links2 = this.getEdges(next_tablename)
-        
+
     //     this.visView.drawSingleTable(tablename, data1["header"], data1["data"], links1, null, visDiv, this.exploreHandler)
     //     this.visView.drawSingleTable(next_tablename, data2["header"], data2["data"], links2, null, visDiv, this.exploreHandler)
 
@@ -138,21 +165,21 @@ export default class VizualizationController {
     exploreHandler = async (d, element) => {
         let data = d["d"]
         let schema = d["schema"]
-        let tablename =  d["tablename"]
+        let tablename = d["tablename"]
         let tableIdx = d["tableIdx"]
 
-        let cur_join_keys = tablename === d.source.id ? d.left_keys[0]: d.left_keys[1]
-        let next_join_keys = tablename === d.source.id ? d.left_keys[1]: d.left_keys[0]
-        let next_tablename = tablename === d.source.id ? d.target.name: d.source.name
+        let cur_join_keys = tablename === d.source.id ? d.left_keys[0] : d.left_keys[1]
+        let next_join_keys = tablename === d.source.id ? d.left_keys[1] : d.left_keys[0]
+        let next_tablename = tablename === d.source.id ? d.target.name : d.source.name
         let selected_join_values = cur_join_keys.map(key => data[schema.indexOf(key)])
         let cur_selection_conds = cur_join_keys.map(key => key + " = " + data[schema.indexOf(key)])
 
 
 
-        let leftTableData = await this.getData(tablename, [], null, null, cur_join_keys, null, selected_join_values)
+        let leftTableData = await this.getData(tablename, [], undefined, [], cur_join_keys, undefined, selected_join_values)
         let projection = {}
         cur_join_keys.map(key => projection[key] = [key, 'IDENTITY'])
-        let joinTable = await this.getData(tablename, [],projection,null, cur_join_keys, null, selected_join_values)
+        let joinTable = await this.getData(tablename, [], projection, [], cur_join_keys, undefined, selected_join_values)
 
         let links1 = this.getEdges(tablename)
         let links2 = this.getEdges(next_tablename)
@@ -161,12 +188,12 @@ export default class VizualizationController {
         let cellHeights = []
         let leftcellHeights = []
         let rightCellHeights = []
-        let cur_join_key_idxs = cur_join_keys.map( key => leftTableData["header"].indexOf(key))
+        let cur_join_key_idxs = cur_join_keys.map(key => leftTableData["header"].indexOf(key))
         let cur_join_key_tuples = leftTableData["data"].map(row => cur_join_key_idxs.map(idx => row[idx]))
         let cur_join_key_set = new Set(cur_join_key_tuples.map(JSON.stringify))
         // get right table data but filter for only join key values
         let next_selection_conds = Array.from(cur_join_key_set).map(JSON.parse).map(tuple => tuple.map((key, idx) => next_join_keys[idx] + " = " + `'${key}'`).join(" AND ")).join(" OR ")
-        let rightTableData = await this.getData(next_tablename, [next_selection_conds],null,null, next_join_keys, null, selected_join_values)
+        let rightTableData = await this.getData(next_tablename, [next_selection_conds], undefined, undefined, next_join_keys, undefined, selected_join_values)
 
         let next_join_key_idxs = next_join_keys.map(key => rightTableData["header"].indexOf(key))
 
@@ -207,7 +234,7 @@ export default class VizualizationController {
         let relationName = elems[i].parentNode.__data__.name
         let data = await this.getHistData(relationName, d)
         this.histView.clear()
-        this.histView.drawHistogram(data,null, relationName, d)
+        this.histView.drawHistogram(data, null, relationName, d)
     }
 
     backgroundClickHandler = () => {
@@ -215,7 +242,7 @@ export default class VizualizationController {
         this.schemaView.unHighlightSchema()
     }
 
-    measSelectionChangeHandler = (relation)=> {
+    measSelectionChangeHandler = (relation) => {
         let node = this.graph.nodes.find((n) => n.name === relation)
         this.measView.populateAttributeDropdown(node)
     }
